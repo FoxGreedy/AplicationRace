954b234a5cf7cf22e28a9d1608fbd42a83661517
FoxGreedy <hotmus8500@gmail.com>
2019-08-29 21:10:43 -0300

    Olá

--- model/competidor.js
+++ model/competidor.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const SchemaCompetidor = new Schema({
     devAdress: { type: String, required: true, unique: true },
     nome: { type: String, required: true },
-    sobrenome: { type: String, require: true },
+    sobrenome: { type: String, required: true },
     peso: { type: Number, required: true },
     corPin: { type: Number, required: true },
     foto: { type: String, required: true },

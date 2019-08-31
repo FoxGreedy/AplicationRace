6bbc4599006f181729bac41b3d8ab6b39678da49
FoxGreedy <hotmus8500@gmail.com>
2019-08-31 11:27:32 -0300

    C:/Program Files/Git/Oi

--- routes/dados.js
+++ routes/dados.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-
 const competidor = require('../model/competidor')
 const dados = require('../model/dados')
 const gps = require('../model/gps')
